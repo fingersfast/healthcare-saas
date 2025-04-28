a63bef03dfa0a52895f74b5b9e74e21852f1b049
fingersfast <himanshu.yengal@gmail.com>
2025-04-28 11:09:55 +0530

    fix:11:09

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   extends: ['next/core-web-vitals'],
   rules: {
-    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { 
+      varsIgnorePattern: 'showAddModal|showEditModal|selectedPrescription' 
+    }],
     'react/no-unescaped-entities': 'warn',
     '@next/next/no-img-element': 'warn',
   },
